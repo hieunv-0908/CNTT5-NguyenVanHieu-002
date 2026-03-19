0ee5679b1e2d10eb5f42cde0baf809328a1d5aa8
hieunv-0908 <nn650370@gmail.com>
2026-03-19 10:06:48 +0700

    Delete HN-K24-CNTT5-NguyenVanHieu_002 directory

--- HN-K24-CNTT5-NguyenVanHieu_002/src/Main.java
+++
@@ -1,9 +0,0 @@
-import presentation.EmployeeManager;
-
-import java.util.*;
-
-public class Main {
-    public static void main(String[] args) {
-        EmployeeManager employeeManager = new EmployeeManager();
-    }
-}
