cf58cf0bf0b5be1b776779c15835a77d39004d17
hieunv-0908 <nn650370@gmail.com>
2026-03-19 10:07:21 +0700

    Add files via upload

---
+++ HN-K24-CNTT5-NguyenVanHieu_002/src/Main.java
@@ -0,0 +1,9 @@
+import presentation.EmployeeManager;
+
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        EmployeeManager employeeManager = new EmployeeManager();
+    }
+}
